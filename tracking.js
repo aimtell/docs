fa852e6031e8fc320f0f746b2e3477b8ac7d8ecd
DJ Swanepoel <djswan@gmail.com>
2026-01-26 15:43:46 -0800

    Add umami

---
+++ tracking.js
@@ -0,0 +1,8 @@
+// Aimtell tracking script
+(function() {
+  var script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://u.aimtell.com/script.js';
+  script.setAttribute('data-website-id', '250f72ec-a4e7-401a-9af5-85b979522336');
+  document.head.appendChild(script);
+})();
